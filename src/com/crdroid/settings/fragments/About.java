30e4bbfe191ef9f776c53d40d2861cbc8e93fcad
ChrisCatto <chriscatto@proton.me>
2025-08-19 21:32:09 +0000

    Settings: Drop donation stuff
    Change-Id: Ie8698985d6ed4c2d5034f48321dc5482c7af9d3d
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- src/com/crdroid/settings/fragments/About.java
+++ src/com/crdroid/settings/fragments/About.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2025 crDroid Android Project
+ * Copyright (C) 2016-2023 crDroid Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,6 +39,7 @@ public class About extends SettingsPreferenceFragment {
 
     public static final String TAG = "About";
 
+    private String KEY_CRDROID_DONATE = "crdroid_donate";
     private String KEY_CRDROID_SOURCE = "crdroid_source";
     private String KEY_CRDROID_TELEGRAM = "crdroid_telegram";
     private String KEY_CRDROID_SHARE = "crdroid_share";
@@ -48,6 +49,7 @@ public class About extends SettingsPreferenceFragment {
     private String KEY_CRDROID_SPONSOR = "crdroid_sponsor";
     private String KEY_CRDROID_BUILDSERVERSPONSOR = "crdroid_buildserversponsor";
 
+    private Preference mDonate;
     private Preference mSourceUrl;
     private Preference mTelegramUrl;
     private Preference mShare;
@@ -62,6 +64,7 @@ public class About extends SettingsPreferenceFragment {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.crdroid_settings_about);
 
+        mDonate = findPreference(KEY_CRDROID_DONATE);
         mSourceUrl = findPreference(KEY_CRDROID_SOURCE);
         mTelegramUrl = findPreference(KEY_CRDROID_TELEGRAM);
         mShare = findPreference(KEY_CRDROID_SHARE);
@@ -74,7 +77,9 @@ public class About extends SettingsPreferenceFragment {
 
     @Override
     public boolean onPreferenceTreeClick(Preference preference) {
-        if (preference == mSourceUrl) {
+        if (preference == mDonate) {
+            launchUrl("https://crdroid.net/donate.php");
+        } else if (preference == mSourceUrl) {
             launchUrl("https://github.com/crdroidandroid");
         } else if (preference == mTelegramUrl) {
             launchUrl("https://t.me/crDroidAndroid");

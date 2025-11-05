aa2a8510824363135dc15c1a37869e61d738faab
priiii08918 <priyanshusinghal0818@proton.me>
2025-11-05 14:30:00 +0000

    Settings: Cleanup brightness slider options
    Change-Id: I9d63a47a88d28be0afc2f97dc7b278058dd1b148

--- src/com/crdroid/settings/fragments/QuickSettings.java
+++ src/com/crdroid/settings/fragments/QuickSettings.java
@@ -17,14 +17,13 @@ package com.crdroid.settings.fragments;
 
 import android.content.ContentResolver;
 import android.content.Context;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.Settings;
 
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
-import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreferenceCompat;
 
@@ -37,7 +36,6 @@ import com.android.settingslib.search.SearchIndexable;
 
 import com.crdroid.settings.fragments.quicksettings.LayoutSettings;
 import com.crdroid.settings.preferences.CustomSeekBarPreference;
-import com.crdroid.settings.utils.DeviceUtils;
 
 import lineageos.providers.LineageSettings;
 
@@ -50,75 +48,15 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
     public static final String TAG = "QuickSettings";
 
-    private static final String QS_BRIGHTNESS_CATEGORY = "qs_brightness_slider_category";
-    private static final String QS_LAYOUT_CATEGORY = "qs_layout_category";
-    private static final String KEY_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
-    private static final String KEY_BRIGHTNESS_SLIDER_POSITION = "qs_brightness_slider_position";
-    private static final String KEY_BRIGHTNESS_SLIDER_HAPTIC = "qs_brightness_slider_haptic";
-    private static final String KEY_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
-    private static final String KEY_QS_TILE_HAPTIC = "qs_tile_haptic";
-
-    private ListPreference mShowBrightnessSlider;
-    private ListPreference mBrightnessSliderPosition;
-    private SwitchPreferenceCompat mBrightnessSliderHaptic;
-    private SwitchPreferenceCompat mShowAutoBrightness;
-    private SwitchPreferenceCompat mQsTileHaptic;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.crdroid_settings_quicksettings);
-
-        final Context context = getContext();
-        final ContentResolver resolver = context.getContentResolver();
-
-        PreferenceCategory brightnessCategory = (PreferenceCategory) findPreference(QS_BRIGHTNESS_CATEGORY);
-        PreferenceCategory tileCategory = (PreferenceCategory) findPreference(QS_LAYOUT_CATEGORY);
-
-        mShowBrightnessSlider = findPreference(KEY_SHOW_BRIGHTNESS_SLIDER);
-        mShowBrightnessSlider.setOnPreferenceChangeListener(this);
-        boolean showSlider = LineageSettings.Secure.getIntForUser(resolver,
-                LineageSettings.Secure.QS_SHOW_BRIGHTNESS_SLIDER, 1, UserHandle.USER_CURRENT) > 0;
-
-        mBrightnessSliderPosition = findPreference(KEY_BRIGHTNESS_SLIDER_POSITION);
-        mBrightnessSliderPosition.setEnabled(showSlider);
-
-        mBrightnessSliderHaptic = findPreference(KEY_BRIGHTNESS_SLIDER_HAPTIC);
-        mQsTileHaptic = findPreference(KEY_QS_TILE_HAPTIC);
-        boolean hapticAvailable = DeviceUtils.hasVibrator(context);
-
-        if (hapticAvailable) {
-            mBrightnessSliderHaptic.setEnabled(showSlider);
-        } else {
-            brightnessCategory.removePreference(mBrightnessSliderHaptic);
-            tileCategory.removePreference(mQsTileHaptic);
-        }
-
-        mShowAutoBrightness = findPreference(KEY_SHOW_AUTO_BRIGHTNESS);
-        boolean automaticAvailable = context.getResources().getBoolean(
-                com.android.internal.R.bool.config_automatic_brightness_available);
-
-        if (automaticAvailable) {
-            mShowAutoBrightness.setEnabled(showSlider);
-        } else {
-            brightnessCategory.removePreference(mShowAutoBrightness);
-        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        ContentResolver resolver = getContext().getContentResolver();
-
-        if (preference == mShowBrightnessSlider) {
-            int value = Integer.parseInt((String) newValue);
-            mBrightnessSliderPosition.setEnabled(value > 0);
-            if (mBrightnessSliderHaptic != null)
-                mBrightnessSliderHaptic.setEnabled(value > 0);
-            if (mShowAutoBrightness != null)
-                mShowAutoBrightness.setEnabled(value > 0);
-            return true;
-        }
         return false;
     }
 
@@ -131,26 +69,5 @@ public class QuickSettings extends SettingsPreferenceFragment implements
      * For search
      */
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider(R.xml.crdroid_settings_quicksettings) {
-
-                @Override
-                public List<String> getNonIndexableKeys(Context context) {
-                    List<String> keys = super.getNonIndexableKeys(context);
-                    final Resources res = context.getResources();
-
-                    boolean automaticAvailable = res.getBoolean(
-                            com.android.internal.R.bool.config_automatic_brightness_available);
-                    if (!automaticAvailable) {
-                        keys.add(KEY_SHOW_AUTO_BRIGHTNESS);
-                    }
-
-                    boolean hapticAvailable = DeviceUtils.hasVibrator(context);
-                    if (!hapticAvailable) {
-                        keys.add(KEY_BRIGHTNESS_SLIDER_HAPTIC);
-                        keys.add(KEY_QS_TILE_HAPTIC);
-                    }
-
-                    return keys;
-                }
-            };
+            new BaseSearchIndexProvider(R.xml.crdroid_settings_quicksettings);
 }

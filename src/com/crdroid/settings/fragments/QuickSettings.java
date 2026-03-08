5a0983015dc02661f136a11deb9171c08e913b28
ChrisCatto <chriscatto@proton.me>
2026-03-08 19:09:05 +0000

    Settings: Hide dual target tile style when minimal QS is enabled
    Change-Id: I54a6be214b3f3c175132bb2443fe46f53adac9ac
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- src/com/crdroid/settings/fragments/QuickSettings.java
+++ src/com/crdroid/settings/fragments/QuickSettings.java
@@ -216,6 +216,10 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                 mQsPanelStyle.setOnPreferenceChangeListener(this);
                 updatePanelStylePrefs(0);
             }
+       }
+
+        if (mDualTargetTileStyle != null) {
+            mDualTargetTileStyle.setVisible(!isMinimalEnabled);
         }
 
         if (mQsTileShape != null) {
@@ -363,6 +367,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
                     if (isMinimalEnabled) {
                         keys.add(KEY_QS_TILE_SHAPE);
+                        keys.add(KEY_DUAL_TARGET_TILE_STYLE);
                     }
 
                     boolean isSliderStyleEnabled = Settings.System.getInt(resolver,

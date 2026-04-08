5455f06f0df26701a5e396385e8696080e211a57
ChrisCatto <chriscatto@proton.me>
2026-04-08 20:29:25 +0300

    Settings: Move some stuff to the correct method
    Change-Id: I628307e29a4d683599c3fff8c07f6141f2ce144c
    Signed-off-by: ChrisCatto <chriscatto@proton.me>

--- src/com/crdroid/settings/fragments/QuickSettings.java
+++ src/com/crdroid/settings/fragments/QuickSettings.java
@@ -154,20 +154,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         if (mSingleQsToneEnabled != null) {
             mSingleQsToneEnabled.setOnPreferenceChangeListener(this);
         }
-    }
-
-    private void updatePanelStylePrefs(int panelStyle) {
-        boolean isClassic = panelStyle == 1;
-
-        if (mQsTileShape != null) {
-            mQsTileShape.setVisible(!isClassic);
-        }
-        if (mQsTileIconShape != null) {
-            mQsTileIconShape.setVisible(isClassic);
-        }
-        if (mQsTileLabelHide != null) {
-            mQsTileLabelHide.setVisible(isClassic);
-        }
 
         mDualTargetTileStyle = findPreference(KEY_DUAL_TARGET_TILE_STYLE);
         if (mDualTargetTileStyle != null) {
@@ -191,6 +177,20 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         if (mQsTileStyleMinimal != null) {
             mQsTileStyleMinimal.setOnPreferenceChangeListener(this);
             updateMinimalStyleDependencies();
+        }
+    }
+
+    private void updatePanelStylePrefs(int panelStyle) {
+        boolean isClassic = panelStyle == 1;
+
+        if (mQsTileShape != null) {
+            mQsTileShape.setVisible(!isClassic);
+        }
+        if (mQsTileIconShape != null) {
+            mQsTileIconShape.setVisible(isClassic);
+        }
+        if (mQsTileLabelHide != null) {
+            mQsTileLabelHide.setVisible(isClassic);
         }
     }
 
